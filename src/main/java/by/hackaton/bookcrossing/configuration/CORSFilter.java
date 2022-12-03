3a844f5a7c94f5b8c398d455ea936da7cf5b0f06
alexei.maiseyenak <a.maiseyenak@gmail.com>
2022-12-03 21:27:37 +0200

    cors

--- src/main/java/by/hackaton/bookcrossing/configuration/CORSFilter.java
+++ src/main/java/by/hackaton/bookcrossing/configuration/CORSFilter.java
@@ -10,10 +10,20 @@ import java.io.IOException;
 @Component
 @Slf4j
 public class CORSFilter implements Filter {
+    private static final String FRONT_URL = "http://localhost:3000"; // URL
+    private static final String BACK_URL = "http://localhost:8181"; // URL
+    private static final String MAIN_URL = "http://staronki.herokuapp.com"; // OTHER URL
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://staronki.herokuapp.com, http://localhost:3000");
+        final HttpServletResponse request = (HttpServletResponse) req;
+        if (FRONT_URL.equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Origin", FRONT_URL);
+        } else if (MAIN_URL.equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Origin", MAIN_URL);
+        } else if (BACK_URL.equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Origin", BACK_URL);
+        }
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         response.setHeader("Access-Control-Max-Age", "3600");
